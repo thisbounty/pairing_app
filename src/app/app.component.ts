821a30a453b70906fbe45b766d64e0e297c03614
Adam Mankowski <maniekmanfred69@gmail.com>
2017-01-06 21:53:53 +0100

    Add menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, App } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 import { MainPage } from '../pages/main/main';
+import { SettingsPage } from '../pages/settings/settings';
 import { SettingsStorage } from '../providers/settings-storage';
 
 
@@ -11,13 +12,24 @@ import { SettingsStorage } from '../providers/settings-storage';
 })
 export class MyApp {
   rootPage = MainPage;
+  nav;
 
-  constructor(platform: Platform, settingsStorage: SettingsStorage) {
+  pages: Array<{title: string, component: any}>;
+
+  constructor(platform: Platform, settingsStorage: SettingsStorage, app: App) {
+    this.pages = [
+      { title: 'Settings', component: SettingsPage },
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+      this.nav = app.getRootNav();
     });
+  }
+  openPage(page){
+    this.nav.push(page.component);
   }
 }
