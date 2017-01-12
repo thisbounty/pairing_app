d40a2d3c296b050a58f9f8973e95cb2abb6930a5
Adam Mankowski <maniekmanfred69@gmail.com>
2017-01-12 20:58:24 +0100

    Make 'setOperates' filter saveable

--- src/modals/settings-modals/set-operates/set-operates.ts
+++ src/modals/settings-modals/set-operates/set-operates.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ViewController } from 'ionic-angular';
+import { ViewController, Events } from 'ionic-angular';
 
 /*
   Generated class for the SetOperates page.
@@ -13,9 +13,20 @@ import { ViewController } from 'ionic-angular';
 })
 export class SetOperatesPage {
 
-  constructor(private viewCtrl: ViewController) {
+  operates_date:string;
+  operates_time:string;
+  until_date:string;
+  until_time:string;
+
+  constructor(private viewCtrl: ViewController, public events: Events) {
   }
-  
+
+  save() {
+    this.events.publish('filter:created', "Set operates filter", { operates_date: this.operates_date, operates_time: this.operates_time,
+                                                                    until_date: this.until_date, until_time: this.until_time});
+    this.viewCtrl.dismiss();
+  }
+
   dismiss(data) {
     this.viewCtrl.dismiss(data);
   }
